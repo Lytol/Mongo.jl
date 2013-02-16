2b4e3e46634a939c744ca84c8f378b7957543230
Brian Smith <bsmith@swig505.com>
2013-02-15 16:44:18 -0800

    Add basic cursor for iterating over collection

--- src/Mongo.jl
+++ src/Mongo.jl
@@ -1,5 +1,8 @@
 module Mongo
 
+const MONGO_SHARED_LIBRARY = "/usr/local/lib/libmongoc"
+
 include("client.jl")
+include("cursor.jl")
 
 end
--- src/client.jl
+++ src/client.jl
@@ -1,12 +1,11 @@
-export Client, close
+export Client
 
 const MONGO_ERROR = -1
-const MONGO_SHARED_LIBRARY = "/usr/local/lib/libmongoc"
 
 type Client
     host::String
     port::Integer
-    mongo::Ptr{Void}
+    _mongo::Ptr{Void}
 
     function Client(host::String, port::Integer)
         p = ccall((:mongo_create, MONGO_SHARED_LIBRARY), Ptr{Void},  ())
@@ -15,9 +14,9 @@ type Client
         end
 
         client = new(host, port, p)
-        finalizer(client, close)
+        finalizer(client, destroy)
 
-        errno = ccall((:mongo_client, MONGO_SHARED_LIBRARY), Int32, (Ptr{Void},Ptr{Uint8},Int32), client.mongo, bytestring(host), port)
+        errno = ccall((:mongo_client, MONGO_SHARED_LIBRARY), Int32, (Ptr{Void},Ptr{Uint8},Int32), client._mongo, bytestring(host), port)
         if errno == MONGO_ERROR
             error("mongo_client error: $(last_error(client))")
         end
@@ -26,10 +25,15 @@ type Client
     end
 end
 
-function close(client::Client)
-    ccall((:mongo_destroy, MONGO_SHARED_LIBRARY), Void, (Ptr{Void},), client.mongo)
+Client() = Client("localhost", 27017)
+
+
+### Private methods ###
+
+function destroy(client::Client)
+    ccall((:mongo_destroy, MONGO_SHARED_LIBRARY), Void, (Ptr{Void},), client._mongo)
 end
 
 function last_error(client::Client)
-    ccall((:mongo_get_err, MONGO_SHARED_LIBRARY), Int32, (Ptr{Void},), client.mongo)
+    ccall((:mongo_get_err, MONGO_SHARED_LIBRARY), Int32, (Ptr{Void},), client._mongo)
 end
