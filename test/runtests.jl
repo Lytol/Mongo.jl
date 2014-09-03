ac48fa746fcdc222af0bdf44a4b06f8c0f62d3b7
Peter Zion <peter.zion@gmail.com>
2014-09-02 22:06:47 -0400

    Add test for bad hostname and port

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,12 @@ facts("Mongo: insert") do
     client = MongoClient()
     collection = MongoCollection(client, "foo", "bar")
     oid = BSONOID()
-    append(document, )
     insert(collection, BSONObject({"_id"=>oid, "hello"=>"before"}))
     update(collection, BSONObject({"_id"=>oid}), BSONObject({"\$set"=>{"hello"=>"after"}}), MongoUpdateFlags.MultiUpdate)
+end
+
+facts("Mongo: bad host/port") do
+    client = MongoClient("bad-host-name", 9999)
+    collection = MongoCollection(client, "foo", "bar")
+    @fact_throws insert(collection, {"foo"=>"bar"})
 end
